adf1711bfc90223f0d2f6a5400bc6db5480a942a
Polina Moskovskaya <polina.moskovskaya@firstlinesoftware.com>
2021-12-21 20:34:18 +0300

    services

---
+++ src/main/java/com/trkpo/blogin/exception/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package com.trkpo.blogin.exception;
+
+public class InvalidPasswordException extends Exception {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/trkpo/blogin/service/PostService.java
@@ -0,0 +1,63 @@
+package com.trkpo.blogin.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trkpo.blogin.converter.PostConverter;
+import com.trkpo.blogin.dto.PostDTO;
+import com.trkpo.blogin.entity.Picture;
+import com.trkpo.blogin.entity.Post;
+import com.trkpo.blogin.entity.User;
+import com.trkpo.blogin.repository.PictureRepository;
+import com.trkpo.blogin.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostService {
+    @Autowired
+    private PictureRepository pictureRepository;
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private PostConverter postConverter;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public void editPost(Post postToEdit, PostDTO post) throws JsonProcessingException {
+        if (post.getPictureId() != null) {
+            Picture pic = pictureRepository.getById(post.getPictureId());
+            postToEdit.setPicture(pic);
+        }
+        postToEdit.setSpan(objectMapper.writeValueAsString(post.getSpan()));
+        postToEdit.setText(post.getText());
+    }
+
+    public List<PostDTO> getPosts(User user) throws JsonProcessingException {
+        List<Post> posts = postRepository.getAllByUserId(user.getId());
+        List<PostDTO> result = new ArrayList<>();
+        for (Post p: posts) {
+            result.add(postConverter.convertPostEntityToDTO(p));
+        }
+        return result;
+    }
+
+    public List<PostDTO> getNews(User user) throws JsonProcessingException {
+        List<User> favourites = user.getFavourites();
+        List<PostDTO> news = new ArrayList<>();
+        for (User u: favourites) {
+            List<Post> posts = postRepository.getAllByUserId(u.getId());
+            for (Post p: posts) {
+                PostDTO postDTO = postConverter.convertPostEntityToDTO(p);
+                if (!postDTO.isPrivate()) {
+                    news.add(postConverter.convertPostEntityToDTO(p));
+                }
+            }
+        }
+        return news.stream().sorted(Comparator.comparing(PostDTO::getDateTime).reversed()).collect(Collectors.toList());
+    }
+}
